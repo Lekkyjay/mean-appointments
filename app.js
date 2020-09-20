007c8d144a68f48019988482a721a12cf46f1448
Lekkyjay <lekkyjayjay@yahoo.com>
2020-09-20 23:16:38 +0200

    Heroku finally works! But mongoClient connection failed. Removed localhost from connection url.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const config = {
   dbCollection: 'appointments'
 }
 
-MongoClient.connect(process.env.MONGODB_URI || `mongodb://${config.dbHost}`, {
+MongoClient.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
